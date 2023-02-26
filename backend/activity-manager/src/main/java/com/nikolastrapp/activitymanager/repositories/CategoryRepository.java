aec290645590bf80868363b3db02c487ba592b7a
NikolasTrapp <nikolastrapp@gmail.com>
2023-02-26 19:40:20 -0300

    Building angular project

--- backend/activity-manager/src/main/java/com/nikolastrapp/activitymanager/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nikolastrapp.activitymanager.repositories;
-
-import com.nikolastrapp.activitymanager.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- backend/activity-manager/src/main/java/com/nikolastrapp/activitymanager/services/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.nikolastrapp.activitymanager.services.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
